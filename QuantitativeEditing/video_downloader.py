2992425c7c21a632ca08ee2d9ba7aea87d6edde2
Walter <wjs018@gmail.com>
2018-11-13 18:50:01 -0500

    Specified video format for compatability.

--- QuantitativeEditing/video_downloader.py
+++ QuantitativeEditing/video_downloader.py
@@ -16,6 +16,7 @@ def download_video(url, output='video', quiet=True):
     ydl_opts['outtmpl'] = output
     ydl_opts['quiet'] = quiet
     ydl_opts['merge_output_format'] = 'mkv'
+    ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio'
     
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
